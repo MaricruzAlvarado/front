d968ced4acd71de9731fa9f93ea0493af74bcdc0
Maricruz <maricruzalvaradoc@outlook.com>
2022-11-21 13:37:13 -0600

    commit 01

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter } from "react-router-dom";
+
+/*
+ *
+ *  Conexión al servidor
+ *
+ */
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql' });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+);
